c0ca9a1d4fed887f2cf8ed84b30af90aca6cddfa
AlexV97 <averville1@gmail.com>
2022-01-30 11:57:03 -0500

    created tower (up to step 32)

--- generate.py
+++ generate.py
@@ -9,6 +9,9 @@ box2_x=1
 box2_y=0
 box2_z=h*3/2
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[box1_x,box1_y,box1_z] , size=[w,l,h])
-pyrosim.Send_Cube(name="Box2", pos=[box2_x,box2_y,box2_z] , size=[l,w,h])
+box_ratio=1
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[box1_x,box1_y,(box1_z+i)] , size=[box_ratio*w,box_ratio*l,box_ratio*h])
+    box_ratio*=0.9
+#pyrosim.Send_Cube(name="Box2", pos=[box2_x,box2_y,box2_z] , size=[l,w,h])
 pyrosim.End()
