16f9c2ff3872e2a8f2b9aa345ccbe781a9479de1
AlexV97 <averville1@gmail.com>
2022-01-30 11:43:00 -0500

    completed up to step 29 of manyLinks

--- generate.py
+++ generate.py
@@ -1,4 +1,14 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+l=1
+w=1
+h=1
+box1_x=0
+box1_y=0
+box1_z=h/2
+box2_x=1
+box2_y=0
+box2_z=h*3/2
+pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Send_Cube(name="Box", pos=[box1_x,box1_y,box1_z] , size=[w,l,h])
+pyrosim.Send_Cube(name="Box2", pos=[box2_x,box2_y,box2_z] , size=[l,w,h])
 pyrosim.End()
