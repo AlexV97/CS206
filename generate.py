d00521d7db23ac13cbf4acfd070fb726507df0a7
AlexV97 <averville1@gmail.com>
2022-01-30 15:14:46 -0500

    created Create_World function

--- generate.py
+++ generate.py
@@ -5,8 +5,10 @@ h=1
 box1_x=0
 box1_y=0
 box1_z=h/2
-
-pyrosim.Start_SDF("world.sdf")
-pyrosim.Send_Cube(name="Box", pos=[box1_x,box1_y,box1_z] , size=[w,l,h])
+class Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[box1_x,box1_y,box1_z] , size=[w,l,h])
     
 pyrosim.End()
+Create_World()
+
