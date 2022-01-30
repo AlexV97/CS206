70d6eeea49372bf1022f0f09fd12fe452f01bd17
AlexV97 <averville1@gmail.com>
2022-01-30 17:35:39 -0500

    completed up to step 19 in joints

--- generate.py
+++ generate.py
@@ -8,7 +8,11 @@ box1_z=h/2
 class Create_World():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(name="Box", pos=[box1_x,box1_y,box1_z] , size=[w,l,h])
+    pyrosim.End()
+class Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[box1_x,box1_y,box1_z] , size=[w,l,h])
+    pyrosim.End()
     
-pyrosim.End()
 Create_World()
-
+Create_Robot()
