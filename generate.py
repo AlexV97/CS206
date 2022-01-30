efefbd97890e3062e9ec71b266958c945d1a33a6
AlexV97 <averville1@gmail.com>
2022-01-30 13:16:44 -0500

    simplified generate.py and changed boxes.sdf to world.sdf

--- generate.py
+++ generate.py
@@ -5,17 +5,8 @@ h=1
 box1_x=0
 box1_y=0
 box1_z=h/2
-box2_x=1
-box2_y=0
-box2_z=h*3/2
 
-pyrosim.Start_SDF("boxes.sdf")
-box_ratio=1
-for k in range(5):
-    for j in range(5):
-        box_ratio=1 # reset box ratio for each column going up
-        for i in range(10): # height
-            pyrosim.Send_Cube(name="Box", pos=[(box1_x+k),(box1_y+j),(box1_z+i)] , size=[box_ratio*w,box_ratio*l,box_ratio*h])
-            box_ratio*=0.9
+pyrosim.Start_SDF("world.sdf")
+pyrosim.Send_Cube(name="Box", pos=[box1_x,box1_y,box1_z] , size=[w,l,h])
     
 pyrosim.End()
