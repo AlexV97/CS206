ee43b5408ba128d88674dded2376f01a4f8e8da8
AlexV97 <averville1@gmail.com>
2022-01-30 13:04:15 -0500

    completed manyLinks

--- generate.py
+++ generate.py
@@ -8,10 +8,14 @@ box1_z=h/2
 box2_x=1
 box2_y=0
 box2_z=h*3/2
+
 pyrosim.Start_SDF("boxes.sdf")
 box_ratio=1
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[box1_x,box1_y,(box1_z+i)] , size=[box_ratio*w,box_ratio*l,box_ratio*h])
-    box_ratio*=0.9
-#pyrosim.Send_Cube(name="Box2", pos=[box2_x,box2_y,box2_z] , size=[l,w,h])
+for k in range(5):
+    for j in range(5):
+        box_ratio=1 # reset box ratio for each column going up
+        for i in range(10): # height
+            pyrosim.Send_Cube(name="Box", pos=[(box1_x+k),(box1_y+j),(box1_z+i)] , size=[box_ratio*w,box_ratio*l,box_ratio*h])
+            box_ratio*=0.9
+    
 pyrosim.End()
