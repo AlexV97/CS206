61588b3f5c2ec36b7a1225db3fb5e1d4f673aebb
AlexV97 <averville1@gmail.com>
2022-01-31 10:40:27 -0500

    removed commented line

--- generate.py
+++ generate.py
@@ -7,7 +7,6 @@ box1_y=0
 box1_z=h/2
 class Create_World():
     pyrosim.Start_SDF("world.sdf")
-#    pyrosim.Send_Cube(name="Box", pos=[(box1_x-4),(box1_y-4),box1_z] , size=[w,l,h])
     pyrosim.Send_Cube(name="Box", pos=[(box1_x-2.0),(box1_y+2.0),box1_z] , size=[w,l,h])
     pyrosim.End()
 class Create_Robot():
