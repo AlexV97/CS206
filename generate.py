417821545f2046dc50e287ae220cab67d3c16f3e
AlexV97 <averville1@gmail.com>
2022-01-31 17:48:02 -0500

    resembling slide 6

--- generate.py
+++ generate.py
@@ -14,7 +14,7 @@ class Create_Robot():
     pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[w,l,h])
     pyrosim.Send_Joint(name = "Torso_Leg" , parent= "Torso" , child = "Leg" ,
     type = "revolute", position = [0.5,0,1.0])
-    pyrosim.Send_Cube(name="Leg", pos=[1.0,0,1.5] , size=[w,l,h])
+    pyrosim.Send_Cube(name="Leg", pos=[0,0,0.5] , size=[w,l,h])
     pyrosim.End()
     
 Create_World()
