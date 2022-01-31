a1263d8f8ef6a66359f51fef210b647154fcccc0
AlexV97 <averville1@gmail.com>
2022-01-30 21:56:16 -0500

    added decimals to coordinates

--- generate.py
+++ generate.py
@@ -1,14 +1,14 @@
 import pyrosim.pyrosim as pyrosim
-l=1
-w=1
-h=1
+l=1.0
+w=1.0
+h=1.0
 box1_x=0
 box1_y=0
 box1_z=h/2
 class Create_World():
     pyrosim.Start_SDF("world.sdf")
 #    pyrosim.Send_Cube(name="Box", pos=[(box1_x-4),(box1_y-4),box1_z] , size=[w,l,h])
-    pyrosim.Send_Cube(name="Box", pos=[(box1_x-2),(box1_y+2),box1_z] , size=[w,l,h])
+    pyrosim.Send_Cube(name="Box", pos=[(box1_x-2.0),(box1_y+2.0),box1_z] , size=[w,l,h])
     pyrosim.End()
 class Create_Robot():
     pyrosim.Start_URDF("body.urdf")
