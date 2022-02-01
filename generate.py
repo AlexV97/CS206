ba0bce573d202d1638ca00cccbaec692e0767bbf
AlexV97 <averville1@gmail.com>
2022-01-31 20:37:55 -0500

    prep for 3-link, 2 joint robot (removing extra blocks)

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ class Create_World():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(name="Box", pos=[(box1_x-2.0),(box1_y+2.0),box1_z] , size=[w,l,h])
     pyrosim.End()
-class Create_Robot():
+class Create_Robot():  # 3-link, 2-joint robot
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5] , size=[w,l,h])
     pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" ,
@@ -18,18 +18,7 @@ class Create_Robot():
     pyrosim.Send_Joint(name = "Link1_Link2" , parent= "Link1" , child = "Link2" ,
     type = "revolute", position = [0,0,1.0])
     pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5] , size=[w,l,h])
-    pyrosim.Send_Joint(name = "Link2_Link3" , parent= "Link2" , child = "Link3" ,
-    type = "revolute", position = [0,0.5,0.5])
-    pyrosim.Send_Cube(name="Link3", pos=[0,0.5,0] , size=[w,l,h])
-    pyrosim.Send_Joint(name = "Link3_Link4" , parent= "Link3" , child = "Link4" ,
-    type = "revolute", position = [0,1.0,0])
-    pyrosim.Send_Cube(name="Link4", pos=[0,0.5,0] , size=[w,l,h])
-    pyrosim.Send_Joint(name = "Link4_Link5" , parent= "Link4" , child = "Link5" ,
-    type = "revolute", position = [0,0.5,-0.5])
-    pyrosim.Send_Cube(name="Link5", pos=[0,0,-0.5] , size=[w,l,h])
-    pyrosim.Send_Joint(name = "Link5_Link6" , parent= "Link5" , child = "Link6" ,
-    type = "revolute", position = [0,0,-1])
-    pyrosim.Send_Cube(name="Link6", pos=[0,0,-0.5] , size=[w,l,h])
+    
     pyrosim.End()
     
 Create_World()
