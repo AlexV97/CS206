3f4a3f6601a70609ad2889bc924f0c55c76f54c5
AlexV97 <averville1@gmail.com>
2022-02-01 20:39:03 -0500

    3-Link, 2-Joint robot (step 47)

--- generate.py
+++ generate.py
@@ -11,14 +11,13 @@ class Create_World():
     pyrosim.End()
 class Create_Robot():  # 3-link, 2-joint robot
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5] , size=[w,l,h])
+    pyrosim.Send_Cube(name="Link0", pos=[0.5,0,0.5] , size=[w,l,h])
     pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" ,
-    type = "revolute", position = [0,0,1.0])
-    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5] , size=[w,l,h])
+    type = "revolute", position = [1.0,0,1.0])
+    pyrosim.Send_Cube(name="Link1", pos=[0.5,0,0.5] , size=[w,l,h])
     pyrosim.Send_Joint(name = "Link1_Link2" , parent= "Link1" , child = "Link2" ,
-    type = "revolute", position = [0,0,1.0])
-    pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5] , size=[w,l,h])
-    
+    type = "revolute", position = [1.0,0,0])
+    pyrosim.Send_Cube(name="Link2", pos=[0.5,0,-0.5] , size=[w,l,h])
     pyrosim.End()
     
 Create_World()
