0604182998caf59e99621db4f914cf9b923f2741
AlexV97 <averville1@gmail.com>
2022-02-07 14:28:09 -0500

    finished sensors assignment

--- analyze.py
+++ analyze.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot
 
 with open('../data/backLegSensorValues.npy','rb') as backLeg_input_file:
     backLegSensorValues = numpy.load(backLeg_input_file)
-    
-matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.plot(backLegSensorValues, label='backLegSensorValues', linewidth=2)
+matplotlib.pyplot.legend()
 
 with open('../data/frontLegSensorValues.npy','rb') as frontLeg_input_file:
     frontLegSensorValues = numpy.load(frontLeg_input_file)
-    
-matplotlib.pyplot.plot(frontLegSensorValues)
+matplotlib.pyplot.plot(frontLegSensorValues, label='frontLegSensorValues', linewidth=3)
+matplotlib.pyplot.legend()
+
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
