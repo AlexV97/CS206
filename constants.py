cf20d8973845b0d15fc55703fd9d5920b06cc228
AlexV97 <averville1@gmail.com>
2022-02-19 16:22:29 -0500

    moved constants to constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+import math
+amplitude_BackLeg = (math.pi)/2
+frequency_BackLeg  = 10
+phaseOffset_BackLeg  = 0
+amplitude_FrontLeg  = (math.pi)/4
+frequency_FrontLeg = 10
+phaseOffset_FrontLeg = (math.pi)/4
+indexRange=1000
--- simulate.py
+++ simulate.py
@@ -1,3 +1,4 @@
+import constants as c
 import pybullet as p
 import pybullet_data
 import time
@@ -7,12 +8,12 @@ import math
 import random
 
 from pathlib import Path
-amplitude_BackLeg = (math.pi)/2
-frequency_BackLeg  = 10
-phaseOffset_BackLeg  = 0
-amplitude_FrontLeg  = (math.pi)/4
-frequency_FrontLeg = 10
-phaseOffset_FrontLeg = (math.pi)/4
+#amplitude_BackLeg = (math.pi)/2
+#frequency_BackLeg  = 10
+#phaseOffset_BackLeg  = 0
+#amplitude_FrontLeg  = (math.pi)/4
+#frequency_FrontLeg = 10
+#phaseOffset_FrontLeg = (math.pi)/4
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -25,16 +26,16 @@ Path.cwd()
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-indexRange=1000
+#indexRange=1000
 
-backLegSensorValues=numpy.zeros(indexRange)
-frontLegSensorValues=numpy.zeros(indexRange)
-targetAngles_BackLeg=numpy.zeros(indexRange)
-targetAngles_FrontLeg=numpy.zeros(indexRange)
+backLegSensorValues=numpy.zeros(c.indexRange)
+frontLegSensorValues=numpy.zeros(c.indexRange)
+targetAngles_BackLeg=numpy.zeros(c.indexRange)
+targetAngles_FrontLeg=numpy.zeros(c.indexRange)
 
-for i in range(indexRange):
-    targetAngles_BackLeg[i]=numpy.sin(math.pi/4)*numpy.sin(((i*2*frequency_BackLeg*math.pi/(indexRange))+phaseOffset_BackLeg))
-    targetAngles_FrontLeg[i]=numpy.sin(math.pi/4)*numpy.sin(((i*2*frequency_FrontLeg*math.pi/(indexRange))+phaseOffset_FrontLeg))
+for i in range(c.indexRange):
+    targetAngles_BackLeg[i]=numpy.sin(math.pi/4)*numpy.sin(((i*2*c.frequency_BackLeg*math.pi/(c.indexRange))+c.phaseOffset_BackLeg))
+    targetAngles_FrontLeg[i]=numpy.sin(math.pi/4)*numpy.sin(((i*2*c.frequency_FrontLeg*math.pi/(c.indexRange))+c.phaseOffset_FrontLeg))
     p.stepSimulation();
     backLegSensorValues[i]=pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i]=pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
@@ -43,14 +44,14 @@ for i in range(indexRange):
     jointName = "Torso_BackLeg",
     controlMode = p.POSITION_CONTROL,
 
-    targetPosition = amplitude_BackLeg * targetAngles_BackLeg[i],
+    targetPosition = c.amplitude_BackLeg * targetAngles_BackLeg[i],
     maxForce = 80)
     pyrosim.Set_Motor_For_Joint(
     bodyIndex = robotId,  #robot,
     jointName = "Torso_FrontLeg",
     controlMode = p.POSITION_CONTROL,
 
-    targetPosition = amplitude_FrontLeg * targetAngles_FrontLeg[i],
+    targetPosition = c.amplitude_FrontLeg * targetAngles_FrontLeg[i],
     maxForce = 80)
     time.sleep(1/480);
 
