28b751a1e297b78deb22ddd12aaf79fca8fc4d3c
AlexV97 <averville1@gmail.com>
2022-02-27 22:17:27 -0500

    neurons - constants clean up

--- constants.py
+++ constants.py
@@ -1,16 +1,8 @@
 import math
 
 # number of steps in simulation
-indexRange=50#2000  # =1000
+indexRange=1000  
 # if common parameters for both legs
 amplitude  = (math.pi)/4
 frequency = 10
 phaseOffset = (math.pi)/4
-## for back leg only
-#amplitude_BackLeg = (math.pi)/2
-#frequency_BackLeg  = 10
-#phaseOffset_BackLeg  = 0
-## for front leg only
-#amplitude_FrontLeg  = (math.pi)/4
-#frequency_FrontLeg = 10
-#phaseOffset_FrontLeg = (math.pi)/4
