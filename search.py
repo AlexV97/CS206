3742dbb4e7c809a20e726ba77a55bb557951d1d0
AlexV97 <averville1@gmail.com>
2022-03-12 21:02:41 -0500

    done up to step 18

--- search.py
+++ search.py
@@ -1,4 +1,9 @@
 import os
-for loop_call in range(5):
-    os.system("python3 generate.py")
-    os.system("python3 simulate.py")
+from hillclimber import HILL_CLIMBER
+import hillclimber as hc
+hc = HILL_CLIMBER()
+#for loop_call in range(5):
+#    os.system("python3 generate.py")
+#    os.system("python3 simulate.py")
+
+
