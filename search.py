f2cb684baaf8472aa38ddbd377fe253ef96c873b
AlexV97 <averville1@gmail.com>
2022-05-01 19:35:06 -0400

    copy result dir with datetime in search.py

--- search.py
+++ search.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
 import parallelHillClimber as phc
 
@@ -9,5 +10,14 @@ phc = PARALLEL_HILL_CLIMBER()
 phc.Evolve()
 phc.Show_Best()
 
+currentDateTime = datetime.now()
+currentDateTime = str(currentDateTime)
+currentDateTime = currentDateTime.replace(":","")
+currentDateTime = currentDateTime.replace(".","")
+currentDateTime = currentDateTime.replace(" ","")
+currentDateTime = currentDateTime.replace("-","")
+print("search.py about to copy ../data_quadruped into../data_quadruped_", currentDateTime)
+os_command_line = "cp -R ../data_quadruped ../data_quadruped_" + currentDateTime
+os.system(os_command_line)
 #os.system("date ")
 
