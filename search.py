10b5a66f0788dbb307b7fb501a097b4982c772c7
AlexV97 <averville1@gmail.com>
2022-05-01 20:21:38 -0400

    created start_multiple calls 10 times search.py

--- search.py
+++ search.py
@@ -1,13 +1,25 @@
 import os
+from datetime import datetime
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
 import parallelHillClimber as phc
 
-#os.system("date ")
+os.system("date > ../data_hexapod/Search_py_startTime.txt")
 
 phc = PARALLEL_HILL_CLIMBER()
 
 phc.Evolve()
 phc.Show_Best()
 
+currentDateTime = datetime.now()
+currentDateTime = str(currentDateTime)
+currentDateTime = currentDateTime.replace(":","")
+currentDateTime = currentDateTime.replace(".","")
+currentDateTime = currentDateTime.replace(" ","")
+currentDateTime = currentDateTime.replace("-","")
+print("search.py about to copy ../data_hexapod into../data_hexapod_", currentDateTime)
+os_command_line = "cp -R ../data_hexapod ../data_hexapod_" + currentDateTime
+os.system(os_command_line)
+
 #os.system("date ")
+
 
---
+++ start_multiple.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("python3 search.py ")
+os.system("python3 search.py ")
+os.system("python3 search.py ")
+os.system("python3 search.py ")
+os.system("python3 search.py ")
+os.system("python3 search.py ")
+os.system("python3 search.py ")
+os.system("python3 search.py ")
+os.system("python3 search.py ")
+os.system("python3 search.py ")
